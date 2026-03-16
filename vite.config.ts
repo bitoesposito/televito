cd8b1563ffe37075d519b021d4a13674aaeecb6f
Vito Esposito <info@vitoesposito.it>
2026-03-16 19:14:37 +0100

    fix: update allowedHosts in vite.config.ts for server and preview to use 'tv.vitoesposito.it'

--- vite.config.ts
+++ vite.config.ts
@@ -11,12 +11,12 @@ export default defineConfig({
   server: {
     host: true,
     port: 3000,
-    allowedHosts: ['vitoesposito.it']
+    allowedHosts: ['tv.vitoesposito.it']
   },
   preview: {
     host: true,
     port: 3000,
-    allowedHosts: ['vitoesposito.it']
+    allowedHosts: ['tv.vitoesposito.it']
   },
   build: {
     // Enable minification for production builds (esbuild is faster than terser)
